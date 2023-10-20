68b7373e1fb795ca753a62b0b0f184cfb8b21a70
William Song <30965609+Freakwill@users.noreply.github.com>
2023-10-20 23:30:32 +0800

    ind

--- pyrimidine/individual.py
+++ pyrimidine/individual.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
+"""
+Individual classes
+"""
 
 from .base import BaseIndividual
 from .chromosome import BinaryChromosome, BaseChromosome, FloatChromosome
