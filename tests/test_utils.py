4b60ae74857bad11ea8d139618b3c70bbb1f6af8
William Song <30965609+Freakwill@users.noreply.github.com>
2023-11-25 10:43:30 +0800

    permutation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,13 +10,13 @@ class TestUtils(unittest.TestCase):
         assert pattern([[0,1,1],[1,1,0]]) == '*1*'
 
     def test_rotation(self):
-        res = rotation([1,2,3,4,5], [2,4,3,5,1])
+        res = rotations([1,2,3,4,5], [2,4,3,5,1])
         assert res == [(0, 1, 3, 4)]
 
-        res = rotation([5,2,3,1,4], [2,5,3,4,1])
+        res = rotations([5,2,3,1,4], [2,5,3,4,1])
         assert res == [(0, 1), (3, 4)]
 
-        assert [2,5,3,4,1] == permutate([5,2,3,1,4], res)
+        assert [2,5,3,4,1] == rotate([5,2,3,1,4], res)
 
     def test_rand(self):
         i, j = randint2()
