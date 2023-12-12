28e8c975453d027afb72967fc5ec0a9219785519
William Song <30965609+Freakwill@users.noreply.github.com>
2023-12-12 11:55:18 +0800

    paper

---
+++ pyrimidine/es.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+(mu + lambda) - Evolution Strategy
+"""
+
+
+from .base import BasePopulation
+from .utils import randint2
+
+
+class EvolutionStrategy(BasePopulation):
+    # Evolution Strategy
+
+    params ={
+        "mu" : 10,
+        "lambda_": 20,
+    }
+
+    def init(self):
+        super().init()
+        if 'mu' not in self.params:
+            self.set_params(mu=self.default_size) 
+
+    def transition(self, *args, **kwargs):
+        offspring = self.mate()
+        self.extend(offspring)
+        self.mate()
+        self.select_best_individuals(self.mu)
+
+    def mate(self, lambda_=None):
+        lambda_ = lambda_ or self.lambda_
+        offspring = []
+        n = len(self)
+        for _ in range(lambda_):
+            i, j = randint2(0, n-1)
+            child = self[i].cross(self[j])
+            offspring.append(child)
+        return offspring
+
+    def select_best_individuals(self, mu=None):
+        mu = mu or self.mu
+        self.individuals = self.get_best_individuals(mu)
